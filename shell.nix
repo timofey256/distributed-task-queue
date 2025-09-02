a38523c84c37764186699868d5c5b2e0177cd88f
timofey256 <shetilin.timofey@gmail.com>
2025-09-02 17:10:49 +0200

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,34 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  haskell = pkgs.haskellPackages;
+in
+pkgs.mkShell {
+  name = "distributed-task-queue-shell";
+
+  buildInputs = [
+    (haskell.ghcWithPackages (p: with p; [
+      # core tools
+      cabal-install
+      haskell-language-server
+
+      # project dependencies (from .cabal file)
+      aeson
+      bytestring
+      text
+      uuid
+      yaml
+      unordered-containers
+      hashable
+      hw-kafka-client
+    ]))
+    pkgs.zlib
+    pkgs.pkg-config
+  ];
+
+  shellHook = ''
+    echo "Welcome to distributed-task-queue dev shell!"
+    echo "Run 'cabal update' once, then 'cabal build'."
+  '';
+}
+
